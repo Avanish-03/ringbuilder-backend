cf2954f2d2fb345dc2f5fdd06dd68fe0242aef0e
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2026-03-12 17:06:50 +0530

    server js file added to solve 500 error

--- index.js
+++ index.js
@@ -1,38 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const app = require("./server");
 
-const diamondsRoutes = require("./Routes/diamonds");
-const settingsRoutes = require("./Routes/settings");
-const customProductsRoutes = require("./Routes/customProducts");
-
-const app = express();
-
-const corsOptions = {
-  origin: [
-    "http://localhost:9292",
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "https://ringsofolight.com",
-    "https://www.ringsofolight.com",
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("HELLO THIS IS THE RING BUILDER");
-});
-
-app.use("/api/diamonds", diamondsRoutes);
-app.use("/api/settings", settingsRoutes);
-app.use("/api/custom-products", customProductsRoutes);
-
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}/`);
 });
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const diamondsRoutes = require("./Routes/diamonds");
+const settingsRoutes = require("./Routes/settings");
+const customProductsRoutes = require("./Routes/customProducts");
+
+const app = express();
+
+const corsOptions = {
+  origin: [
+    "http://localhost:9292",
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://ringsofolight.com",
+    "https://www.ringsofolight.com",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("HELLO THIS IS THE RING BUILDER");
+});
+
+app.use("/api/diamonds", diamondsRoutes);
+app.use("/api/settings", settingsRoutes);
+app.use("/api/custom-products", customProductsRoutes);
+
+module.exports = app;
