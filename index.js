c2cfe70d4830682ff592be5be54309a877615f84
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2026-03-12 16:59:35 +0530

    port

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use("/api/diamonds", diamondsRoutes);
 app.use("/api/settings", settingsRoutes);
 app.use("/api/custom-products", customProductsRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}/`);
 });
