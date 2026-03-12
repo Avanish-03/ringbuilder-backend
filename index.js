e8ba787e26ffd52e80e840e051f75ab52d3d3ebb
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2026-03-12 16:55:11 +0530

    CORS origin set

--- index.js
+++ index.js
@@ -8,7 +8,20 @@ const customProductsRoutes = require("./Routes/customProducts");
 
 const app = express();
 
-app.use(cors({ origin: true }));
+const corsOptions = {
+  origin: [
+    "http://localhost:9292",
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://ringsofolight.com",
+    "https://www.ringsofolight.com",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -19,7 +32,7 @@ app.use("/api/diamonds", diamondsRoutes);
 app.use("/api/settings", settingsRoutes);
 app.use("/api/custom-products", customProductsRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}/`);
 });
