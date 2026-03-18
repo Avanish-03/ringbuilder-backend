91a03fa75471428b20a5db0409f5ef753b7a5e65
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2026-03-18 12:09:49 +0530

    solving the publishing product

--- Controller/createCustomProduct.js
+++ Controller/createCustomProduct.js
@@ -8,6 +8,20 @@ const shopify = axios.create({
   },
 });
 
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const graphqlRequest = async (query, variables = {}) => {
+  const response = await shopify.post("", { query, variables });
+
+  if (response.data.errors) {
+    const error = new Error("Shopify GraphQL error");
+    error.details = response.data.errors;
+    throw error;
+  }
+
+  return response.data.data;
+};
+
 const createCustomProduct = async (req, res) => {
   try {
     const { diamondId, shopify_variant_id, price, title, image } = req.body;
@@ -19,13 +33,13 @@ const createCustomProduct = async (req, res) => {
       });
     }
 
-    // 1. Create product
     const createProductMutation = `
       mutation CreateProduct($product: ProductCreateInput!) {
         productCreate(product: $product) {
           product {
             id
             title
+            status
           }
           userErrors {
             field
@@ -42,20 +56,12 @@ const createCustomProduct = async (req, res) => {
       },
     };
 
-    const productRes = await shopify.post("", {
-      query: createProductMutation,
-      variables: createProductVariables,
-    });
+    const productCreateData = await graphqlRequest(
+      createProductMutation,
+      createProductVariables
+    );
 
-    if (productRes.data.errors) {
-      return res.status(500).json({
-        success: false,
-        step: "productCreate",
-        graphqlErrors: productRes.data.errors,
-      });
-    }
-
-    const productData = productRes.data.data.productCreate;
+    const productData = productCreateData.productCreate;
 
     if (productData.userErrors.length) {
       return res.status(400).json({
@@ -67,7 +73,6 @@ const createCustomProduct = async (req, res) => {
 
     const productId = productData.product.id;
 
-    // 2. Get default variant
     const variantQuery = `
       query GetProductVariant($productId: ID!) {
         product(id: $productId) {
@@ -80,21 +85,10 @@ const createCustomProduct = async (req, res) => {
       }
     `;
 
-    const variantRes = await shopify.post("", {
-      query: variantQuery,
-      variables: { productId },
-    });
-
-    if (variantRes.data.errors) {
-      return res.status(500).json({
-        success: false,
-        step: "getDefaultVariant",
-        graphqlErrors: variantRes.data.errors,
-      });
-    }
+    const variantData = await graphqlRequest(variantQuery, { productId });
 
     const defaultVariantId =
-      variantRes.data.data.product?.variants?.nodes?.[0]?.id;
+      variantData.product?.variants?.nodes?.[0]?.id || null;
 
     if (!defaultVariantId) {
       return res.status(500).json({
@@ -104,7 +98,6 @@ const createCustomProduct = async (req, res) => {
       });
     }
 
-    // 3. Update variant price
     const updateMutation = `
       mutation UpdateVariant($productId: ID!, $variants: [ProductVariantsBulkInput!]!) {
         productVariantsBulkUpdate(productId: $productId, variants: $variants) {
@@ -133,20 +126,8 @@ const createCustomProduct = async (req, res) => {
       ],
     };
 
-    const updateRes = await shopify.post("", {
-      query: updateMutation,
-      variables: updateVariables,
-    });
-
-    if (updateRes.data.errors) {
-      return res.status(500).json({
-        success: false,
-        step: "variantUpdate",
-        graphqlErrors: updateRes.data.errors,
-      });
-    }
-
-    const updateData = updateRes.data.data.productVariantsBulkUpdate;
+    const updateResult = await graphqlRequest(updateMutation, updateVariables);
+    const updateData = updateResult.productVariantsBulkUpdate;
 
     if (updateData.userErrors.length) {
       return res.status(400).json({
@@ -156,7 +137,6 @@ const createCustomProduct = async (req, res) => {
       });
     }
 
-    // 4. Add product image
     if (image) {
       const mediaMutation = `
         mutation CreateMedia($productId: ID!, $media: [CreateMediaInput!]!) {
@@ -187,20 +167,8 @@ const createCustomProduct = async (req, res) => {
         ],
       };
 
-      const mediaRes = await shopify.post("", {
-        query: mediaMutation,
-        variables: mediaVariables,
-      });
-
-      if (mediaRes.data.errors) {
-        return res.status(500).json({
-          success: false,
-          step: "mediaCreate",
-          graphqlErrors: mediaRes.data.errors,
-        });
-      }
-
-      const mediaData = mediaRes.data.data.productCreateMedia;
+      const mediaResult = await graphqlRequest(mediaMutation, mediaVariables);
+      const mediaData = mediaResult.productCreateMedia;
 
       if (mediaData.mediaUserErrors.length) {
         return res.status(400).json({
@@ -211,17 +179,78 @@ const createCustomProduct = async (req, res) => {
       }
     }
 
+    const publicationsQuery = `
+      query GetPublications {
+        publications(first: 20) {
+          nodes {
+            id
+            name
+          }
+        }
+      }
+    `;
+
+    const publicationsData = await graphqlRequest(publicationsQuery);
+    const onlineStorePublication = publicationsData.publications?.nodes?.find((pub) =>
+      /online store/i.test(pub.name)
+    );
+
+    if (!onlineStorePublication) {
+      return res.status(500).json({
+        success: false,
+        step: "getPublication",
+        message: "Online Store publication not found",
+      });
+    }
+
+    const publishMutation = `
+      mutation PublishProduct($id: ID!, $input: [PublicationInput!]!) {
+        publishablePublish(id: $id, input: $input) {
+          publishable {
+            ... on Product {
+              id
+            }
+          }
+          shop {
+            id
+          }
+          userErrors {
+            field
+            message
+          }
+        }
+      }
+    `;
+
+    const publishData = await graphqlRequest(publishMutation, {
+      id: productId,
+      input: [{ publicationId: onlineStorePublication.id }],
+    });
+
+    const publishResult = publishData.publishablePublish;
+
+    if (publishResult.userErrors.length) {
+      return res.status(400).json({
+        success: false,
+        step: "publishProduct",
+        userErrors: publishResult.userErrors,
+      });
+    }
+
+    await sleep(2000);
+
     return res.json({
       success: true,
-      message: "Custom product created successfully",
+      message: "Custom product created and published successfully",
       productId,
       variantId: defaultVariantId,
+      publicationId: onlineStorePublication.id,
     });
   } catch (error) {
     return res.status(500).json({
       success: false,
       message: "FAILED",
-      error: error.response?.data || error.message,
+      error: error.details || error.response?.data || error.message,
     });
   }
 };
